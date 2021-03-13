4a08dddac566730dfa25b1525fa9bb3dc093696e
Ian Rugg <ian@olokoo.com>
2021-03-13 16:52:38 -0700

    Responsive updates to journal entries.

--- src/components/SectionDiaryEntries.js
+++ src/components/SectionDiaryEntries.js
@@ -11,7 +11,7 @@ export default class SectionDiaryEntries extends React.Component {
         let entries_to_show = _.get(section, 'entries_to_show', null) || 6;
         return (
             <section id={_.get(section, 'section_id', null)} className="posts">
-                <div className="container py-5">
+                <div className="container py-2 py-small-5">
                     {((_.get(section, 'show_section_heading', null) === true) && _.get(section, 'section_heading_text', null)) && (
                         <div className="row">
                             <div className="col-100 text-align-center">
@@ -21,7 +21,7 @@ export default class SectionDiaryEntries extends React.Component {
                     )}
                     <div className="row">
                         {_.map(_.orderBy(_.take(display_posts, entries_to_show), 'frontmatter.date', 'desc'), (post, post_idx) => (
-                            <div className="col-1-2" key={post_idx}>
+                            <div className="col-100 col-small-1-2" key={post_idx}>
                                 <div className="diary-entry">
                                     <h2>Dear Diary,</h2>
                                     <h4 className="publish-date">{moment(_.get(post, 'frontmatter.date', null)).strftime('%B %d, %Y')}</h4>
