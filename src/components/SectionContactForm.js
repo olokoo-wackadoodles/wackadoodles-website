8ad7507164fc233a483613c07722a00e149de7f5
Stackbit Code Editor <projects@stackbit.com>
2021-03-14 19:12:12 +0000

    SectionContactForm.js: changed

--- src/components/SectionContactForm.js
+++ src/components/SectionContactForm.js
@@ -17,27 +17,27 @@ export default class SectionContactForm extends React.Component {
                 )}
                 <div className="row">
                     <div className="col-100">
-                    <form name={_.get(section, 'form_name', null) || 'contact'} method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                        <input type="hidden" name="bot-field" />
-                        <input type="hidden" name="form-name" value="contact" />
-                        <div className="field half first">
-                            <label className="h4" htmlFor="name">Enter your name</label>
-                            <input type="text" name="name" id="name" />
-                        </div>
-                        <div className="field half">
-                            <label className="h4" htmlFor="email">Enter your email address</label>
-                            <input type="text" name="email" id="email" />
-                        </div>
-                        <div className="field">
-                            <label className="h4" htmlFor="message">Enter your message to Mrs.Wackadoodle</label>
-                            <textarea name="message" id="message" rows="6" />
-                        </div>
-                        <ul className="actions text-align-center">
-                            <li>
-                                <input type="submit" value="Send Message" className="primary button-link" />
-                            </li>
-                        </ul>
-                    </form>
+                        <form name={_.get(section, 'form_name', null) || 'contact'} method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                            <input type="hidden" name="bot-field" />
+                            <input type="hidden" name="form-name" value="contact" />
+                            <div className="field half first">
+                                <label className="h4" htmlFor="name">Enter your name</label>
+                                <input type="text" name="name" id="name" />
+                            </div>
+                            <div className="field half">
+                                <label className="h4" htmlFor="email">Enter your email address</label>
+                                <input type="text" name="email" id="email" />
+                            </div>
+                            <div className="field">
+                                <label className="h4" htmlFor="message">Enter your message to Mrs.Wackadoodle</label>
+                                <textarea name="message" id="message" rows="6" />
+                            </div>
+                            <ul className="actions text-align-center">
+                                <li>
+                                    <input type="submit" value="Send Message" className="primary button-link" />
+                                </li>
+                            </ul>
+                        </form>
             </div>
         );
     }
