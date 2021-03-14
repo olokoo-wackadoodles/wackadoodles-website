d18a153c9bfe1501c0e4abf092c857dfc7815fd6
Stackbit Code Editor <projects@stackbit.com>
2021-03-14 19:12:16 +0000

    SectionContactForm.js: changed

--- src/components/SectionContactForm.js
+++ src/components/SectionContactForm.js
@@ -38,6 +38,8 @@ export default class SectionContactForm extends React.Component {
                                 </li>
                             </ul>
                         </form>
+                    </div>
+                </div>
             </div>
         );
     }
